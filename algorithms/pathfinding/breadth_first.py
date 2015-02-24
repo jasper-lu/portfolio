b53bd7d0d6431b9e06c6e5720d6236e9bfc24a44
Jasper Lu <repsajrules@gmail.com>
2015-02-23 18:21:11 -0800

    finished up the search algo

--- algorithms/pathfinding/breadth_first.py
+++ algorithms/pathfinding/breadth_first.py
@@ -1,4 +1,4 @@
-from graph import Grid, Queue
+from graph import Grid, Queue, trace
 
 def breadth_first_search(graph, start, end):
     frontier = Queue()
@@ -8,14 +8,17 @@ def breadth_first_search(graph, start, end):
 
     while not frontier.empty():
         current = frontier.get()
+
         if current == end:
-            print(came_from[current])
             break
 
         for x in graph.neighbors(current):
-            frontier.put(x)
-            came_from[x] = current
+            if x not in came_from:
+                frontier.put(x)
+                came_from[x] = current
+
+    return trace(came_from, end)
 
 if __name__ == "__main__":
     graph = Grid(3,3)
-    breadth_first_search(graph, (0,0), (3,3))
+    print(breadth_first_search(graph, (0,0), (1,2)))
