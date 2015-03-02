077c223c981b268650c77961bb3bec76cdab8520
Jasper Lu <repsajrules@gmail.com>
2015-03-02 10:27:54 -0800

    bugs all ironed out, added print function

--- data_structures/linked_list/src/linked_list.c
+++ data_structures/linked_list/src/linked_list.c
@@ -9,8 +9,8 @@ static LNode_t* createLNode(void *data);
 //frees a node and returns the pointer to the data
 static void* freeLNode(LNode_t* node);
 
-LList_t createLList(void (*destroy)(void* data)) {
-  LList_t list = malloc(sizeof(LList_t));
+LList_t *createLList(void (*destroy)(void* data)) {
+  LList_t* list = malloc(sizeof(LList_t));
   list->len = 0;
   list->head = list->tail = NULL;
   list->destroy = destroy;
@@ -18,13 +18,13 @@ LList_t createLList(void (*destroy)(void* data)) {
 }
 
 void freeLList(LList_t* list) {
-  LNode* curr = list->head;
+  LNode_t *curr = list->head;
   while (curr != NULL) {
-    LNode* next = head->next;
+    LNode_t *next = curr->next;
     freeLNode(curr);
     curr = next;
-  }
   free(list);
+  }
 }
 
 void insert(LList_t* list, void* data) {
@@ -33,10 +33,14 @@ void insert(LList_t* list, void* data) {
   LNode_t* newFront = createLNode(data);
   newFront->next = front;
   list->head = newFront;
+
+  if (list->tail == NULL) {
+    list->tail = newFront;
+  }
 }
 
 void insertAtIndex(LList_t* list, int index, void* data) {
-  if (index > 0 && index < list->len -1) {
+  if (index > 0 && index < list->len) {
     LNode_t* insertAfter = list->head;
     //i want to get to the node before the index and insert after
     while (index > 1) {
@@ -46,7 +50,7 @@ void insertAtIndex(LList_t* list, int index, void* data) {
     insertAfterLNode(list, insertAfter, data);
   } else if (index == 0) {
     insert(list, data);
-  } else if (index == list->len - 1) {
+  } else if (index == list->len) {
     append(list, data);
   }
 }
@@ -56,23 +60,40 @@ void insertAfterLNode(LList_t* list, LNode_t* node, void* data) {
   LNode_t* newNode = createLNode(data);
   newNode->next = node->next;
   node->next = newNode;
+
+  if (node == list->tail) {
+    list->tail = newNode;
+  }
 }
 
 void append(LList_t* list, void* data) {
   list->len++;
   LNode_t* node = createLNode(data);
   list->tail->next = node;
+  list->tail = node;
 }
 
 void* removeFirst(LList_t* list) {
   list->len--;
   LNode_t* toRemove = list->head;
   list->head = toRemove->next;
+
+  if (toRemove == list->tail) {
+    list->tail = NULL;
+  }
+
   return freeLNode(list->head);
 }
 
 //possibly the slowest running function in this data struct
 void* removeLast(LList_t* list) {
+  //if there's only one node left, it's much easier to do it this way
+  if (list->len == 1) {
+    LNode_t* node = list->tail;
+    list->len--;
+    list->head = list->tail = NULL;
+    return freeLNode(node);
+  }
   return removeAtIndex(list, list->len - 1);
 }
 
@@ -88,30 +109,38 @@ void* removeAtIndex(LList_t* list, int index) {
   } else if (index == 0) {
     return removeFirst(list);
   }
-  return;
+  return NULL;
 }
 
 //will assume that the node passed will never be tail
-void *removeAfterLNode(LList_t* list, LNode_t* node) {
+void *removeAfterLNode(LList_t *list, LNode_t *node) {
   list->len--;
-  LNode_t* toRemove = node->next;
+  LNode_t *toRemove = node->next;
   if (toRemove == list->tail) {
-    list->tail = toRemove;
+    list->tail = node;
   }
-
   node->next = toRemove->next;
   return freeLNode(toRemove);
 }
 
-static LNode_t* createLNode(void* data) {
+static LNode_t* createLNode(void *data) {
   LNode_t* node = malloc(sizeof(LNode_t));
   node->data = data;
   node->next = NULL;
   return node;
 }
 
-static void* freeLNode(LNode_t* node) {
+static void* freeLNode(LNode_t *node) {
   void* data = node->data;
   free(node);
   return data;
+}
+
+void printLList(LList_t *list) {
+  LNode_t *curr = list->head;
+  while(curr != NULL) {
+    printf("%s->", (char *)curr->data);
+    curr = curr->next;
+  }
+  printf("NULL\n");
 }
--- data_structures/linked_list/src/linked_list.h
+++ data_structures/linked_list/src/linked_list.h
@@ -1,39 +1,40 @@
 typedef struct LNode {
-  void* data;
-  struct LNode* data;
-  LNode_t* next;
+  void *data;
+  struct LNode *next;
 } LNode_t;
 
-typedef struct {
+typedef struct LList {
   int len;
-  struct LNode_t* head, tail;
+  LNode_t *head, *tail;
   void (*destroy)(void *data);
 } LList_t;
 
-LList_t createLList(void (*destroy)(void* data));
+LList_t *createLList(void (*destroy)(void *data));
 
-void freeLList(LList_t* list);
+void freeLList(LList_t *list);
 
 //insert in the front
-void insert(LList_t* list, void* data);
+void insert(LList_t *list, void *data);
 
 //insert at a certain index
-void insertAtIndex(LList_t* list, int index, void* data);
+void insertAtIndex(LList_t *list, int index, void *data);
 
 //insert after a node
-void insertAfterLNode(LList_t* list, LNode_t* node, void* data);
+void insertAfterLNode(LList_t *list, LNode_t *node, void *data);
 
 //insert at end
-void append(LList_t* list, void* data);
+void append(LList_t *list, void* data);
 
 //remove from front
-void *removeFirst(LList_t* list);
+void *removeFirst(LList_t *list);
 
 //remove from end
-void *removeLast(LList_t* list);
+void *removeLast(LList_t *list);
 
 //remove from index
-void *removeAtIndex(LList_t* list, int index);
+void *removeAtIndex(LList_t *list, int index);
 
 //remove after a node
-void *removeAfterLNode(LList_t* list, LNode_t* node);
+void *removeAfterLNode(LList_t *list, LNode_t *node);
+
+void printLList(LList_t* list);
