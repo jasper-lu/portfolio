170a5d8206698884a03caa0c7ba5b35d19632f57
Jasper Lu <repsajrules@gmail.com>
2015-03-02 10:28:01 -0800

    added tests

---
+++ data_structures/linked_list/src/test.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linked_list.h"
+
+static void freeData(void* data) {
+  free((char* )data);
+}
+
+static void testLList() {
+  char* chars[4];
+  int i;
+  for (i = 0; i < 4; ++i) {
+    chars[i] = malloc(2);
+    sprintf(chars[i], "%d", i);
+  }
+
+  LList_t* list = createLList(freeData);
+  insert(list, chars[0]);
+  insertAtIndex(list, 1, chars[1]);
+  insertAfterLNode(list, list->tail, chars[2]);
+  append(list, chars[3]);
+
+  printLList(list);
+
+  removeFirst(list);
+  removeAtIndex(list, 1);
+  removeAfterLNode(list, list->head);
+  removeLast(list);
+
+  printLList(list);
+}
+
+int main() {
+  testLList();
+  return 1;
+}
